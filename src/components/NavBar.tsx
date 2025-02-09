64519da59117e6d951c05cb2721cf9a6a5fd7bfc
Blissmal <malutibethuel@gmail.com>
2025-02-09 18:25:52 +0300

    Nav render

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,11 @@
 const NavBar = () => {
   return (
     <div className="sticky top-0 bg-white z-index-10">
-      
+      <div className="container hidden lg:block">
+        <div className="flex justify-between items-center p-8">
+            <h1 className="text-4xl font-medium">Logo</h1>
+        </div>
+      </div>
     </div>
   )
 }
