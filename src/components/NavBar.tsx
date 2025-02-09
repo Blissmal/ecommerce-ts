8ebd6057cbbdf19d4304428822c3ed388222f01e
Blissmal <malutibethuel@gmail.com>
2025-02-09 23:37:23 +0300

    Updated code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { AiOutlineUser } from "react-icons/ai"
 import { BsSearch } from "react-icons/bs"
 
 const NavBar = () => {
@@ -9,6 +10,11 @@ const NavBar = () => {
             <div className="relative w-full max-w-[500px]">
               <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[32px] w-full" type="text" placeholder="Search product .." />
               <BsSearch size={20} className="absolute top-0 right-0 mt-4 mr-5 text-gray-500"/>
+            </div>
+            <div className="flex gap-4">
+              <div className="icon__wrapper">
+                <AiOutlineUser />
+              </div>
             </div>
         </div>
       </div>
